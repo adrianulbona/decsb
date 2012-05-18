0d5c02846ceabf07488df90775d0d7969e73ae22
Adrian Bona <adrianulbona@mail.com>
2012-05-18 10:52:49 +0300

    tests preparation

---
+++ src/ro/utcluj/ecsb/EcsbTests.java
@@ -0,0 +1,29 @@
+package ro.utcluj.ecsb;
+
+import org.apache.log4j.Logger;
+import ro.utcluj.ecsb.utils.EcsbClassifiers;
+import ro.utcluj.ecsb.utils.EcsbFactory;
+import ro.utcluj.ecsb.utils.EcsbUtils;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Properties;
+
+public class EcsbTests {
+    public static void main(String[] args){
+        try{
+            for(EcsbClassifiers classifier : EcsbClassifiers.values()){
+                System.out.println(classifier.getClassName() + " - starting time:" + new Timestamp(System.currentTimeMillis()));
+                Properties configuration = EcsbUtils.loadConfiguration(false, ECSB.CONF_PATH + "decsb.properties");
+                EcsbUtils.initLogger(false, "nd_"+classifier.getClassName()+".txt");
+                configuration.setProperty("base_classifier", classifier.getClassName());
+                final ECSB ecsb = new EcsbFactory(configuration).setUpECSB(false);
+                ecsb.runEvolutionaryCostSensitiveBalancing();
+                System.out.println(classifier.getClassName() + " - end time:" + new Timestamp(System.currentTimeMillis()));
+            }
+        } catch (IOException e) {
+            Logger.getLogger("ECSBLog").error("Unable to load configuration file.");
+    }
+
+    }
+}
--- src/ro/utcluj/ecsb/utils/EcsbClassifiers.java
+++ src/ro/utcluj/ecsb/utils/EcsbClassifiers.java
@@ -3,9 +3,9 @@ package ro.utcluj.ecsb.utils;
 public enum EcsbClassifiers {
     IBk("weka.classifiers.lazy.IBk"),
     J48("weka.classifiers.trees.J48"),
-    NaiveBayes("weka.classifiers.bayes.NaiveBayes"),
-    AdaBoostM1("weka.classifiers.meta.AdaBoostM1");
-    //SMO("weka.classifiers.functions.SMO");
+    //NaiveBayes("weka.classifiers.bayes.NaiveBayes"),
+    AdaBoostM1("weka.classifiers.meta.AdaBoostM1"),
+    SMO("weka.classifiers.functions.SMO");
 
     private final String className;
 
